8bf77061fd7fbdd35bf688666efde2420c48ae1a
dmswls0062 <dmswls0062@naver.com>
2024-01-19 18:05:25 +0900

    정산그래프

--- src/main/java/test/spring/mvc/controller/MemberController.java
+++ src/main/java/test/spring/mvc/controller/MemberController.java
@@ -33,6 +33,11 @@ public class MemberController {
 
 		return "member/all";
 	}
+	@RequestMapping("seller")
+	public String seller() {
+		return "member/seller";
+	}
+	
 	
 	//카카오 로그인 
 	@RequestMapping("check")
--- src/main/java/test/spring/mvc/service/SellerSaleServiceImpl.java
+++ src/main/java/test/spring/mvc/service/SellerSaleServiceImpl.java
@@ -25,4 +25,59 @@ public class SellerSaleServiceImpl implements SellerSaleService{
 		sellermapper.updateTrackingAndCourier( tracking, courier ,orderid, companyid );
 	}
 
+	@Override
+	public Integer  getTodaySales(String companyid) {
+		companyid+="%";
+		System.out.println(companyid);
+		return sellermapper.getTodaySales(companyid);
+	}
+
+	@Override
+	public Integer  getWeeklySales(String companyid) {
+		companyid+="%";
+		return sellermapper.getWeeklySales(companyid);
+	}
+
+	@Override
+	public Integer  getMonthlySales(String companyid) {
+		companyid+="%";
+		return sellermapper.getMonthlySales(companyid);
+	}
+
+	@Override
+	public Integer todayNetSales(String companyid) {
+		companyid+="%";
+		return sellermapper.todayNetSales(companyid);
+	}
+
+	@Override
+	public Integer weeklyNetSales(String companyid) {
+		companyid+="%";
+		return sellermapper.weeklyNetSales(companyid);
+	}
+
+	@Override
+	public Integer monthlyNetSales(String companyid) {
+		companyid+="%";
+		return sellermapper.monthlyNetSales(companyid);
+	}
+
+	@Override
+	public Integer todayFee(String companyid) {
+		companyid+="%";
+		return sellermapper.todayFee(companyid);
+	}
+
+	@Override
+	public Integer weeklyFee(String companyid) {
+		companyid+="%";
+		return sellermapper.weeklyFee(companyid);
+	}
+
+	@Override
+	public Integer monthlyFee(String companyid) {
+		companyid+="%";
+		return sellermapper.monthlyFee(companyid);
+	}
+
 }
